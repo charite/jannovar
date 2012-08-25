86b840ed0874ea489ae85b2874d2c25b62ceaea3
Peter Robinson <Peter.Robinson@charite.de>
2012-08-25 06:53:41 +0000

    Refactoring
    git-svn-id: https://compbio.charite.de/public-svn/NGSanalysis@663 a7cbf6ab-7213-4657-a3ac-d80d95007299

---
+++ Exomizer/src/exomizer/exome/Exome.java
@@ -0,0 +1,20 @@
+package exomizer.exome;
+
+import java.util.ArrayList;
+
+import exomizer.exome.Variant;
+
+
+public class Exome {
+
+    private ArrayList<Variant> list_of_variants=null;
+
+
+
+    public Exome(ArrayList<Variant> vars) {
+	this.list_of_variants = vars;
+    }
+
+
+
+}
