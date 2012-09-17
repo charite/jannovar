f515834ebea9d2d0456e13ca828ce8105126ba68
Peter Robinson <Peter.Robinson@charite.de>
2012-09-17 19:40:09 +0000

    Fixed bug with junit program
    git-svn-id: https://compbio.charite.de/public-svn/NGSanalysis@730 a7cbf6ab-7213-4657-a3ac-d80d95007299

--- Exomizer/junit/src/exomizer/tests/UCSCKGParserTest.java
+++ Exomizer/junit/src/exomizer/tests/UCSCKGParserTest.java
@@ -16,61 +16,43 @@ import exomizer.common.Constants;
 
 
 import org.junit.Test;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import junit.framework.Assert;
 
 
 public class UCSCKGParserTest implements Constants {
 
-    private static UCSCKGParser parser = null;
+    private UCSCKGParser parser = null;
     private ArrayList<KGLine> knownGeneList=null;
 
-    @BeforeClass public static void setUp() throws IOException {
+    @Before public void setUp() throws IOException {
 	System.out.println("ABout");
 	File tmp = File.createTempFile("ucsckg-test","ucsckg-test");
-	PrintStream ps = new PrintStream(new FileOutputStream(tmp));
-	ps.append("uc009vis.3	chr1	-	14361	16765	14361	14361	4	14361,14969,15795,16606,	14829,15038,15942,16765,		uc009vis.3\n");
-	ps.close();
-	String mypath="/home/peter/data/ucsc";
-	//parser = new UCSCKGParser(tmp.getAbsolutePath());
-	parser = new UCSCKGParser(mypath);
-	
-	
-
-    }
-
-    /*    @Before
-	public void setUp() throws IOException
-    {
-	System.out.println("ABout");
-	File tmp = File.createTempFile("ucsckg-test","ucsckg-test");
-	System.out.println("ABout to read." +tmp.getAbsolutePath());
 	PrintStream ps = new PrintStream(new FileOutputStream(tmp));
 	ps.append("uc009vis.3	chr1	-	14361	16765	14361	14361	4	14361,14969,15795,16606,	14829,15038,15942,16765,		uc009vis.3\n");
 	ps.append("uc009viv.2	chr1	-	14406	29370	14406	14406	7	14406,16857,17232,17605,17914,24737,29320,	16765,17055,17368,17742,18061,24891,29370,		uc009viv.2\n");
-
 	ps.close();
-
-	System.out.println("ABout to parse." +tmp.getAbsolutePath());
-	this.parser = new UCSCKGParser(tmp.getAbsolutePath());
+	//String mypath="/home/peter/data/ucsc/knownGene.txt";
+	parser = new UCSCKGParser(tmp.getAbsolutePath());
 	parser.parseFile();
 	this.knownGeneList = parser.getKnownGeneList();
     }
 
-    */
     @Test public void testSizeOfKGList() {
-	try {
-	    parser.parseFile();
-	} catch (Throwable e) {
-	    System.out.println("EX");//e.printTrackTrace();
-	}
-	int N = 3;//this.knownGeneList.size();
-	Assert.assertEquals(1,N);	  
+
+	int N = this.knownGeneList.size();
+	Assert.assertEquals(2,N);	  
     }
- @Test public void testSizeOfKGLis2t() {
-	int N = 3;//this.knownGeneList.size();
-	Assert.assertEquals(1,N);	  
+
+    /**
+     *uc009vis.3 is the gene WASH7P and has a lenght of 843 nt. */
+    @Test public void testGene_uc009vis() {
+	KGLine kgl = knownGeneList.get(0);
+	int len = kgl.getMRNALength();
+	Assert.assertEquals(843,len);
+
     }
+
 
 
 
