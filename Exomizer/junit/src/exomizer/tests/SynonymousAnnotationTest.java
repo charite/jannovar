013ff0c48a0c85b939c84d980ff292b6c633def4
Peter Robinson <Peter.Robinson@charite.de>
2012-12-10 17:21:34 +0000

    Addding synonymous test
    git-svn-id: https://compbio.charite.de/public-svn/NGSanalysis@820 a7cbf6ab-7213-4657-a3ac-d80d95007299

---
+++ Exomizer/junit/src/exomizer/tests/SynonymousAnnotationTest.java
@@ -0,0 +1,112 @@
+package exomizer.tests;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/* serialization */
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+import exomizer.io.UCSCKGParser;
+import exomizer.common.Constants;
+import exomizer.io.AnnovarParser;
+import exomizer.reference.KnownGene;
+import exomizer.reference.Chromosome;
+import exomizer.reference.Annotation;
+import exomizer.exome.Variant;
+import exomizer.exception.AnnotationException;
+
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import junit.framework.Assert;
+
+
+/**
+ * This class is intended to perform unuit testing on variants that
+ * are intergenic. 
+ */
+public class SynonymousAnnotationTest implements Constants {
+
+    
+   
+    private static HashMap<Byte,Chromosome> chromosomeMap = null;
+
+  
+
+    @SuppressWarnings (value="unchecked")
+    @BeforeClass 
+    public static void setUp() throws IOException {
+	HashMap<String,KnownGene> kgMap=null;
+	// The following file must be created prior to running this test
+	String serializedFile = "../ucsc.ser";
+	try {
+	     FileInputStream fileIn =
+		 new FileInputStream(serializedFile);
+	     ObjectInputStream in = new ObjectInputStream(fileIn);
+	     kgMap = (HashMap<String,KnownGene>) in.readObject();
+            in.close();
+            fileIn.close();
+	} catch(IOException i) {
+            i.printStackTrace();
+	    System.err.println("Could not deserialize knownGeneMap");
+	    System.exit(1);
+           
+        } catch(ClassNotFoundException c) {
+            System.out.println("Could not find HashMap<String,KnownGene> class.");
+            c.printStackTrace();
+            System.exit(1);
+        }
+	chromosomeMap = new HashMap<Byte,Chromosome> ();
+	for (KnownGene kgl : kgMap.values()) {
+	    byte chrom = kgl.getChromosome();
+	    if (! chromosomeMap.containsKey(chrom)) {
+		Chromosome chr = new Chromosome(chrom);
+		chromosomeMap.put(chrom,chr);
+	    }
+	    Chromosome c = chromosomeMap.get(chrom);
+	    c.addGene(kgl);	
+	}
+    }
+
+
+
+
+
+/**
+ *<P>
+ * annovar: RNF207:uc001amg.3:exon17:c.1718A>G:p.N573S,
+ * chr1:6278414A>G
+ *</P>
+ */
+@Test public void testSynVar1hand() throws AnnotationException  {
+	byte chr = 1;
+	int pos = 897738;
+	String ref = "C";
+	String alt = "T";
+	Chromosome c = chromosomeMap.get(chr); 
+	if (c==null) {
+	    Assert.fail("Could not identify chromosome \"" + chr + "\"");
+	} else {
+	    Annotation ann =c.getAnnotation(pos,ref,alt); 
+	    byte varType = ann.getVarType();
+	    Assert.assertEquals(SYNONYMOUS,varType);
+	    String annot = ann.getVariantAnnotation();
+	    Assert.assertEquals("KLHL17(uc001aca.2:exon5:c.715C>T:p.L239L)",annot);
+	}
+}
+
+ 	
+
+
+
+
+}
