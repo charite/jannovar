1c69d39f1fa233eca71e278ace686bf408eb51c8
Peter Robinson <Peter.Robinson@charite.de>
2013-05-20 18:08:26 +0000

    Adapting pom file to create executable
    git-svn-id: https://compbio.charite.de/public-svn/NGSanalysis@1481 a7cbf6ab-7213-4657-a3ac-d80d95007299

---
+++ jannovar/runJannovar.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+## Test run of Jannovar
+## TO create the serialized UCSC file, you need to download data files from the UCSC
+## server, http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/
+## There are four files to be downloaded (KnownGene.txt, knownGeneMrna.txt, kgXref.txt
+## and knownToLocusLink.txt). Put them all in some directory (probably you then need 
+## to change the following path):
+DIR="/home/peter/data/ucsc"
+
+## The following four variables now give the complete path of these four files
+KG="${DIR}/knownGene.txt"
+MRNA="${DIR}/knownGeneMrna.txt" 
+XR="${DIR}/kgXref.txt"
+LOCUS="${DIR}/knownToLocusLink.txt"
+
+## This is the default name of the output file
+SERIAL="ucsc.ser"
+
+##set -x  uncomment for VERBOSE
+
+JAVA="/usr/bin/java"
+MV="/bin/mv"
+
+JANNOVAR="target/jannovar-0.0.5-SNAPSHOT.jar"
+
+
+echo ${JAVA}  -Xms1G -Xmx2G -jar ${JANNOVAR} -U $KG -M $MRNA -X $XR -L $LOCUS -S $SERIAL
+${JAVA}  -Xms1G -Xmx2G -jar ${JANNOVAR} -U $KG -M $MRNA -X $XR -L $LOCUS -S $SERIAL
