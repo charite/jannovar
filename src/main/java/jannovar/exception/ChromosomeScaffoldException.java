5ac508ed40739c2562e12facc8e5c5873bfddf7a
Peter Robinson <peter.robinson@charite.de>
2013-07-12 16:49:23 +0200

    Addding chromosome scafffold exception

---
+++ src/main/java/jannovar/exception/ChromosomeScaffoldException.java
@@ -0,0 +1,31 @@
+package jannovar.exception;
+
+
+
+/**
+ * Exception that occurs during parsing of VCF files.
+ * @author Peter Robinson 
+ * @version 0.03 (April 28,2013)
+ */
+public class ChromosomeScaffoldException extends VCFParseException {
+
+    public static final long serialVersionUID = 2L;
+
+    public String badChromosome=null;
+
+    /** Record when there is an unparsable chromosome, usually
+     * a scaffold such as GL000225.1
+     */
+    public void setBadChromosome(String c) { this.badChromosome = c; }
+
+    public String getBadChromosome() { return this.badChromosome; }
+
+    public ChromosomeScaffoldException() {
+	super("Unknown exception during parsing of VCF File");
+    }
+
+    public ChromosomeScaffoldException(String msg) {
+	super(msg);
+    }
+
+}
