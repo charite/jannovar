8f0b2b815635d867d97611f67a5202be4b17afdf
Seb <dr.sebastian.koehler@gmail.com>
2014-01-06 13:25:19 +0100

    fixed package-declaration bug, that would not have happened with eclipse

--- src/test/java/jannovar/genotype/GenotypeCallTest.java
+++ src/test/java/jannovar/genotype/GenotypeCallTest.java
@@ -1,44 +1,37 @@
-package jannovar.io;
+package jannovar.genotype;
 
+import jannovar.common.Genotype;
 
 import java.util.ArrayList;
 
-import jannovar.genotype.GenotypeCall;
-import jannovar.common.Genotype;
-
-import org.junit.Test;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Assert;
-
+import org.junit.Test;
 
 /**
  * Test adding and retireving data about the genotype.
  */
 public class GenotypeCallTest {
 
+	@Test
+	public void testGetQuality() {
+		ArrayList<Genotype> calls = new ArrayList<Genotype>();
+		calls.add(Genotype.HETEROZYGOUS);
+		ArrayList<Integer> qualities = new ArrayList<Integer>();
+		qualities.add(100);
+		GenotypeCall gc = new GenotypeCall(calls, qualities);
+		int q = gc.getQualityInIndividualN(0);
+		Assert.assertEquals(100, q);
+	}
 
-     @Test public void testGetQuality()  
-    {
-	ArrayList<Genotype> calls = new ArrayList<Genotype> ();
-	calls.add(Genotype.HETEROZYGOUS);
-	ArrayList<Integer> qualities = new ArrayList<Integer> ();
-	qualities.add(100);
-	GenotypeCall gc = new GenotypeCall(calls,qualities);
-	int q = gc.getQualityInIndividualN(0);
-	Assert.assertEquals(100,q);
-    }
-
-     @Test public void testGetGenotype()  
-    {
-	ArrayList<Genotype> calls = new ArrayList<Genotype> ();
-	calls.add(Genotype.HETEROZYGOUS);
-	ArrayList<Integer> qualities = new ArrayList<Integer> ();
-	qualities.add(100);
-	GenotypeCall gc = new GenotypeCall(calls,qualities);
-	Genotype g = gc.getGenotypeInIndividualN(0);
-	Assert.assertEquals(Genotype.HETEROZYGOUS,g);
-    }
-
+	@Test
+	public void testGetGenotype() {
+		ArrayList<Genotype> calls = new ArrayList<Genotype>();
+		calls.add(Genotype.HETEROZYGOUS);
+		ArrayList<Integer> qualities = new ArrayList<Integer>();
+		qualities.add(100);
+		GenotypeCall gc = new GenotypeCall(calls, qualities);
+		Genotype g = gc.getGenotypeInIndividualN(0);
+		Assert.assertEquals(Genotype.HETEROZYGOUS, g);
+	}
 
 }
