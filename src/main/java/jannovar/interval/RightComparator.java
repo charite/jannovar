89528690e93c26c3b34a9cc74e4dda7190cd4462
Seb <dr.sebastian.koehler@gmail.com>
2014-02-14 13:03:43 +0100

    trying to revise the whole left/right comparator stuff, things were getting blurry with all the static/subclass things. first try to improve this

--- src/main/java/jannovar/interval/RightComparator.java
+++ src/main/java/jannovar/interval/RightComparator.java
@@ -2,42 +2,39 @@ package jannovar.interval;
 
 import java.util.Comparator;
 
-
-
 /**
- * This class is intended to be used to sort
- * lists of {@link jannovar.interval.Interval Interval} objects by descending
- * order of their right high-points.
+ * This class is intended to be used to sort lists of
+ * {@link jannovar.interval.Interval Interval} objects by descending order of
+ * their right high-points.
+ * 
  * @author names
  * @version 0.02 (15 May, 2013)
  * @param <T>
  */
-public class RightComparator<T> implements Comparator<Interval<T>>  {
-    
-    @Override
-    public int compare(Interval<T> interval_1, Interval<T> interval_2) {
-	/*
-	 * returns -1 if the highpoint of i is bigger than the highpoint of
-	 * j
-	 */
+public class RightComparator<T extends Interval<T>> implements Comparator<Interval<?>> {
+	// compare(Interval<?> interval_1, Interval<?> interval_2) {
+	@Override
+	public int compare(Interval<?> interval_1, Interval<?> interval_2) {
+		/*
+		 * returns -1 if the highpoint of i is bigger than the highpoint of j
+		 */
 
-	if (interval_1.getHigh() > interval_2.getHigh())
-	    return -1;
-	/*
-	 * returns 1 if the highpoint of i is smaller than the highpoint of
-	 * j
-	 */
-	else if (interval_1.getHigh() < interval_2.getHigh() )
-	    return 1;
-	/* returns -1 if the lowpoint of i is bigger than the lowpoint of j */
-	else if (interval_1.getLow() > interval_2.getLow())
-	    return -1;
-	/* returns 1 if the lowpoint of i is smaller than the lowpoint of j */
-	else if (interval_1.getLow() < interval_2.getLow() )
-	    return 1;
-	/* returns 0 if they are equal */
-	else
-	    return 0;
-	
-    }
+		if (interval_1.getHigh() > interval_2.getHigh())
+			return -1;
+		/*
+		 * returns 1 if the highpoint of i is smaller than the highpoint of j
+		 */
+		else if (interval_1.getHigh() < interval_2.getHigh())
+			return 1;
+		/* returns -1 if the lowpoint of i is bigger than the lowpoint of j */
+		else if (interval_1.getLow() > interval_2.getLow())
+			return -1;
+		/* returns 1 if the lowpoint of i is smaller than the lowpoint of j */
+		else if (interval_1.getLow() < interval_2.getLow())
+			return 1;
+		/* returns 0 if they are equal */
+		else
+			return 0;
+
+	}
 }
--- src/test/java/jannovar/interval/NodeTest.java
+++ src/test/java/jannovar/interval/NodeTest.java
@@ -1,60 +1,47 @@
 package jannovar.interval;
 
-
-
-import java.util.Collections;
-import java.util.ArrayList;
-
-
-import jannovar.interval.Interval;
-import jannovar.interval.LeftComparator;
-import jannovar.interval.RightComparator;
-import jannovar.interval.Node;
-
 import jannovar.exception.IntervalTreeException;
 
-import org.junit.Test;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Assert;
+import java.util.ArrayList;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
  * Tests the Interval class.
  */
 
 public class NodeTest {
-    
-    /** Median should be 3,4,5,17,19,20,21,55,90,100 => 19? */
-    private ArrayList<Interval<String>>  createIntervalList1() throws IntervalTreeException {
-	Interval<String> i1 = new Interval<String>(3,100,"A");
-        Interval<String> i2 = new Interval<String>(7,90,"B");
-        Interval<String> i3 = new Interval<String>(4,55,"C");
-        Interval<String> i4 = new Interval<String>(20,21,"D");
-        Interval<String> i5 = new Interval<String>(17,19,"E");
-        ArrayList<Interval<String>>  lst = new  ArrayList<Interval<String>>();
-        lst.add(i1);
-        lst.add(i2);
-        lst.add(i3);
-        lst.add(i4);
-        lst.add(i5);
-	return lst;
-    }
-    
-    @BeforeClass 
-    public static void setUp() {
-	Node.setLeftComparator(new LeftComparator());
-	Node.setRightComparator(new RightComparator());
-    }
-    
-    
-    
- @Test public void testNodeConstruction1()  throws IntervalTreeException
-    {
-	ArrayList<Interval<String>>  lst = createIntervalList1();
-	Node n = new Node(lst);
-	Integer m = n.getMedian();
-        Assert.assertEquals((Integer)19,m);
-    }
+
+	/** Median should be 3,4,5,17,19,20,21,55,90,100 => 19? */
+	private ArrayList<Interval<String>> createIntervalList1() throws IntervalTreeException {
+		Interval<String> i1 = new Interval<String>(3, 100, "A");
+		Interval<String> i2 = new Interval<String>(7, 90, "B");
+		Interval<String> i3 = new Interval<String>(4, 55, "C");
+		Interval<String> i4 = new Interval<String>(20, 21, "D");
+		Interval<String> i5 = new Interval<String>(17, 19, "E");
+		ArrayList<Interval<String>> lst = new ArrayList<Interval<String>>();
+		lst.add(i1);
+		lst.add(i2);
+		lst.add(i3);
+		lst.add(i4);
+		lst.add(i5);
+		return lst;
+	}
+
+	@BeforeClass
+	public static void setUp() {
+		// Node.setLeftComparator(new LeftComparator());
+		// Node.setRightComparator(new RightComparator());
+	}
+
+	@Test
+	public void testNodeConstruction1() throws IntervalTreeException {
+		ArrayList<Interval<String>> lst = createIntervalList1();
+		Node n = new Node(lst, new LeftComparator(), new RightComparator());
+		Integer m = n.getMedian();
+		Assert.assertEquals((Integer) 19, m);
+	}
 
 }
