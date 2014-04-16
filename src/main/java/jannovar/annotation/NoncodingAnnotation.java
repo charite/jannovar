e3ba5459dc5178f77b94632ab3860cecb9d46192
mjaeger <mjaeger@medhunter.charite.de>
2014-04-16 11:59:46 +0200

    short HGSV, minor chnages

--- src/main/java/jannovar/annotation/NoncodingAnnotation.java
+++ src/main/java/jannovar/annotation/NoncodingAnnotation.java
@@ -4,53 +4,64 @@ import jannovar.common.VariantType;
 import jannovar.reference.TranscriptModel;
 
 /**
- * This class provides static methods to generate annotations for non-coding
- * RNA variants. 
- * @version 0.03 (7 July, 2013)
- * @author Peter N Robinson
+ * This class provides static methods to generate annotations for non-coding RNA variants.
+ * 
+ * @version 0.04 (15 April, 2014)
+ * @author Peter N Robinson, Marten Jäger
  */
 
 public class NoncodingAnnotation {
 
+	/**
+	 * Return an annotation for a UTR3 mutation for a gene.
+	 * 
+	 * @param trmdl
+	 *            Gene with splice mutation for current chromosomal variant.
+	 * @param rvarstart
+	 *            start position of the variant
+	 * @param ref
+	 *            reference sequence
+	 * @param alt
+	 *            variant sequence
+	 * @param exonNumber
+	 *            number of the exon affected by the variant (zero-based).
+	 * @return An {@link jannovar.annotation.Annotation Annotation} object corresponding to a 3' UTR variant.
+	 */
+	public static Annotation createNoncodingExonicAnnotation(TranscriptModel trmdl, int rvarstart, String ref, String alt, int exonNumber) {
+		// shift
+		if (!(ref.length() == 1 && alt.length() == 1 && !alt.equals("-"))) { // only for non singlenucleotide
+																				// substitutions
+			while (trmdl.getCdnaSequence().charAt(rvarstart - 1) == trmdl.getCdnaSequence().charAt(rvarstart + ref.length() - 1)) {
+				rvarstart++;
+				ref = new StringBuilder().append(ref.substring(1)).append(ref.charAt(0)).toString();
+			}
+		}
 
-    
- /**
-     * Return an annotation for a UTR3  mutation for a gene.
-     * @param trmdl Gene with splice mutation for current chromosomal variant.
-     * @param rvarstart start position of the variant
-     * @param ref reference sequence
-     * @param alt variant sequence
-     * @param exonNumber number of the exon affected by the variant (zero-based).
-     * @return An {@link jannovar.annotation.Annotation Annotation} object corresponding to a 3' UTR variant.
-     */
-    public static Annotation createNoncodingExonicAnnotation(TranscriptModel trmdl, int rvarstart, String ref, String alt,int exonNumber) {
-	String annotation = null;
-	exonNumber++; /* correct from zero-based to one-based numbering */
-	if (alt.equals("-")) {
-	   /* i.e., deletion  */
-	    if (ref.length() == 1) {
-		annotation = String.format("%s:exon%d:n.%ddel%s",trmdl.getName(),exonNumber,rvarstart,ref);
-	    } else {
-		int d2 = rvarstart + ref.length() - 1;
-		annotation = String.format("%s:exon%d:n.%d_%ddel%s",trmdl.getName(),exonNumber,rvarstart,d2,ref);
-		    } 
-	} else if (ref.equals("-")) {
-	    /* i.e., insertion */
-	    int d2=rvarstart + 1; /* get end of insertion */ 
-	    annotation = String.format("%s:exon%d:n.%d_%dins%s",trmdl.getName(),exonNumber,rvarstart,d2,alt);
-	   
-	} else {
-	    /* i.e., substitution. The following code will be correct for SNPs, and may need to 
-	       be improved in the future for block substitutions. */
-	    annotation = String.format("%s:exon%d:n.%d%s>%s",trmdl.getName(),exonNumber,rvarstart,ref,alt);
-	   
+		String annotation = null;
+		exonNumber++; /* correct from zero-based to one-based numbering */
+		if (alt.equals("-")) {
+			/* i.e., deletion  */
+			if (ref.length() == 1) {
+				annotation = String.format("%s:exon%d:n.%ddel", trmdl.getName(), exonNumber, rvarstart);
+			} else {
+				int d2 = rvarstart + ref.length() - 1;
+				annotation = String.format("%s:exon%d:n.%d_%ddel", trmdl.getName(), exonNumber, rvarstart, d2);
+			}
+		} else if (ref.equals("-")) {
+			/* i.e., insertion */
+			int d2 = rvarstart + 1; /* get end of insertion */
+			annotation = String.format("%s:exon%d:n.%d_%dins%s", trmdl.getName(), exonNumber, rvarstart, d2, alt);
+
+		} else {
+			/* i.e., substitution. The following code will be correct for SNPs, and may need to 
+			   be improved in the future for block substitutions. */
+			annotation = String.format("%s:exon%d:n.%d%s>%s", trmdl.getName(), exonNumber, rvarstart, ref, alt);
+
+		}
+		Annotation ann = new Annotation(trmdl, annotation, VariantType.ncRNA_EXONIC, rvarstart);
+		ann.setGeneID(trmdl.getGeneID());
+		ann.setGeneSymbol(trmdl.getGeneSymbol());
+		return ann;
 	}
-	Annotation ann = new Annotation(trmdl, annotation, VariantType.ncRNA_EXONIC,rvarstart);
-	ann.setGeneID( trmdl.getGeneID() );
-	ann.setGeneSymbol( trmdl.getGeneSymbol() );
-	return ann;
-    }
-
-
 
 }
