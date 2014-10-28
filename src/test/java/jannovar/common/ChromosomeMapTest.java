bdbe72be27fc717196496677f3a4b1f47d32db5d
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2014-10-28 12:10:36 +0100

    Adding tests for ChromosomeMap.

---
+++ src/test/java/jannovar/common/ChromosomeMapTest.java
@@ -0,0 +1,19 @@
+package jannovar.common;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Tests for the ChromosomeMap interface.
+ */
+public class ChromosomeMapTest {
+
+	@Test
+	public void testIdentifier2Chromosom() {
+		Assert.assertEquals(ChromosomeMap.identifier2chromosom.get("chr1").intValue(), 1);
+		Assert.assertEquals(ChromosomeMap.identifier2chromosom.get("chrM").intValue(), ChromosomeMap.M_CHROMOSOME);
+		Assert.assertEquals(ChromosomeMap.identifier2chromosom.get("NC_000020.11").intValue(), 20);
+		Assert.assertEquals(ChromosomeMap.identifier2chromosom.get("NC_000082.5").intValue(), 16);
+	}
+}
