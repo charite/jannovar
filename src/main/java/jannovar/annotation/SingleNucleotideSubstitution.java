93c9a1016c67e5940aef7390cbd2ec8fb567e8b8
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2014-10-30 11:23:25 +0100

    Refactoring SingleNucleotideSubstitution.

--- src/main/java/jannovar/annotation/SingleNucleotideSubstitution.java
+++ src/main/java/jannovar/annotation/SingleNucleotideSubstitution.java
@@ -16,7 +16,7 @@ import jannovar.reference.Translator;
  * there is a discrepancy between the genomic sequence (which is the sequence that is typically used for variant calling
  * and thus informs the variant calls in the VCF file) and the UCSC mRNA sequence. We no longer throw an Exception in
  * this case, as in versions of this class up to the 15th of December, 2012 (v. 0.04).
- * 
+ *
  * @version 0.12 (15 April 2014)
  * @author Peter N Robinson, Marten Jäger
  */
@@ -28,42 +28,42 @@ public class SingleNucleotideSubstitution {
 	 * This function decides what strand the affected {@link jannovar.reference.TranscriptModel TranscriptModel} is
 	 * located on and calls either {@link #getAnnotationPlusStrand} or {@link #getAnnotationMinusStrand} to do the
 	 * calculations.
-	 * 
+	 *
 	 * @param kgl
 	 *            The known gene that corresponds to the deletion caused by the variant.
-	 * @param frame_s
+	 * @param frameShift
 	 *            0 if variant begins at first base of codon, 1 if it begins at second base, 2 if at third base (same
 	 *            for SNV)
-	 * @param frame_end_s
+	 * @param frameEndShift
 	 *            0 Frame at end of variant
 	 * @param wtnt3
 	 *            Nucleotide sequence of wildtype codon
-	 * 
+	 *
 	 * @param ref
 	 *            sequence of wildtype sequence
 	 * @param var
 	 *            alternate sequence (should be '-')
-	 * @param refvarstart
+	 * @param refVarStart
 	 *            Position of the variant in the CDS of the known gene
 	 * @param exonNumber
 	 *            Number of the affected exon (zero-based).
 	 * @return An annotation corresponding to the deletion.
 	 * @throws jannovar.exception.AnnotationException
 	 */
-	public static Annotation getAnnotation(TranscriptModel kgl, int frame_s, int frame_end_s, String wtnt3, String ref, String var, int refvarstart, int exonNumber) throws AnnotationException {
+	public static Annotation getAnnotation(TranscriptModel kgl, int frameShift, int frameEndShift, String wtnt3, String ref, String var, int refVarStart, int exonNumber) throws AnnotationException {
 		if (kgl.isPlusStrand())
-			return getAnnotationPlusStrand(kgl, frame_s, wtnt3, ref, var, refvarstart, exonNumber);
+			return getAnnotationPlusStrand(kgl, frameShift, wtnt3, ref, var, refVarStart, exonNumber);
 		else
-			return getAnnotationPlusStrand(kgl, frame_s, wtnt3, ref, var, refvarstart, exonNumber);
+			return getAnnotationPlusStrand(kgl, frameShift, wtnt3, ref, var, refVarStart, exonNumber);
 
 	}
 
 	/**
 	 * Creates annotation for a single-nucleotide substitution on the plus strand.
-	 * 
-	 * @param kgl
+	 *
+	 * @param tm
 	 *            The known gene that corresponds to the deletion caused by the variant.
-	 * @param frame_s
+	 * @param frameShift
 	 *            0 if deletion begins at first base of codon, 1 if it begins at second base, 2 if at third base
 	 * @param wtnt3
 	 *            Nucleotide sequence of wildtype codon
@@ -71,99 +71,102 @@ public class SingleNucleotideSubstitution {
 	 *            sequence of wildtype sequence
 	 * @param var
 	 *            alternate sequence (should be '-')
-	 * @param refvarstart
+	 * @param refVarStart
 	 *            Position of the variant in the CDS of the known gene
 	 * @param exonNumber
 	 *            Number of the affected exon (zero-based).
 	 * @return An annotation corresponding to the deletion.
 	 * @throws jannovar.exception.AnnotationException
 	 */
-	public static Annotation getAnnotationPlusStrand(TranscriptModel kgl, int frame_s, String wtnt3, String ref, String var, int refvarstart, int exonNumber) throws AnnotationException {
+	public static Annotation getAnnotationPlusStrand(TranscriptModel tm, int frameShift, String wtnt3, String ref,
+			String var, int refVarStart, int exonNumber) throws AnnotationException {
 		Translator translator = Translator.getTranslator(); /* Singleton */
-		String canno = null; // cDNA annotation.
-		String panno = null;
-		String varnt3 = null;
-		int refcdsstart = kgl.getRefCDSStart(); /* position of start codon in transcript. */
-		int cdspos = refvarstart - refcdsstart + 1; /* position of mutation in CDS, numbered from start codon */
+		String cDNAAnno = null; // cDNA annotation.
+		String protAnno = null;
+		String varNT3 = null;
+		int refCDSStart = tm.getRefCDSStart(); /* position of start codon in transcript. */
+		int cdsPos = refVarStart - refCDSStart + 1; /* position of mutation in CDS, numbered from start codon */
 
-		Annotation ann = null;
-		VariantType vartype = null;
+		VariantType varType = null;
 		String wrng = null;
 
 		if (ref.length() != 1) {
-			throw new AnnotationException(String.format("Error: Malformed reference sequence (%s) for SNV annotation of %s", ref, kgl.getGeneSymbol()));
+			throw new AnnotationException(String.format(
+					"Error: Malformed reference sequence (%s) for SNV annotation of %s", ref, tm.getGeneSymbol()));
 		} else if (var.length() != 1) {
-			throw new AnnotationException(String.format("Error: Malformed variant sequence (%s) for SNV annotation of %s", var, kgl.getGeneSymbol()));
+			throw new AnnotationException(String.format(
+					"Error: Malformed variant sequence (%s) for SNV annotation of %s", var, tm.getGeneSymbol()));
 		}
 		char refc = ref.charAt(0);
 		char varc = var.charAt(0);
-		if (frame_s == 1) {
+		if (frameShift == 1) {
 			// $varnt3 = $wtnt3[0] . $obs . $wtnt3[2];
-			varnt3 = String.format("%c%c%c", wtnt3.charAt(0), varc, wtnt3.charAt(2));
+			varNT3 = String.format("%c%c%c", wtnt3.charAt(0), varc, wtnt3.charAt(2));
 			// $canno = "c.$wtnt3[1]" . ($refvarstart-$refcdsstart+1) . $obs;
-			canno = String.format("c.%d%c>%c", (refvarstart - refcdsstart + 1), wtnt3.charAt(1), varc);
+			cDNAAnno = String.format("c.%d%c>%c", (refVarStart - refCDSStart + 1), wtnt3.charAt(1), varc);
 			if (refc != wtnt3.charAt(1)) {
-				char strand = kgl.getStrand();
+				char strand = tm.getStrand();
 				wrng = String.format("WARNING:_mRNA/genome_discrepancy:_%s/%s_strand=%c", ref, wtnt3.charAt(1), strand);
 			}
-		} else if (frame_s == 2) {
+		} else if (frameShift == 2) {
 			// $varnt3 = $wtnt3[0] . $wtnt3[1]. $obs;
-			varnt3 = String.format("%c%c%c", wtnt3.charAt(0), wtnt3.charAt(1), varc);
+			varNT3 = String.format("%c%c%c", wtnt3.charAt(0), wtnt3.charAt(1), varc);
 			// $canno = "c." . ($refvarstart-$refcdsstart+1) . $wtnt3[2] . ">" . $obs;
-			canno = String.format("c.%d%c>%c", (refvarstart - refcdsstart + 1), wtnt3.charAt(2), varc);
+			cDNAAnno = String.format("c.%d%c>%c", (refVarStart - refCDSStart + 1), wtnt3.charAt(2), varc);
 			if (refc != wtnt3.charAt(2)) {
-				char strand = kgl.getStrand();
+				char strand = tm.getStrand();
 				wrng = String.format("WARNING:_mRNA/genome_discrepancy:_%s/%s_strand=%c", ref, wtnt3.charAt(1), strand);
 			}
 		} else { /* i.e., frame_s == 0 */
 			// $varnt3 = $obs . $wtnt3[1] . $wtnt3[2];
-			varnt3 = String.format("%c%c%c", varc, wtnt3.charAt(1), wtnt3.charAt(2));
+			varNT3 = String.format("%c%c%c", varc, wtnt3.charAt(1), wtnt3.charAt(2));
 			// $canno = "c." . ($refvarstart-$refcdsstart+1) . $wtnt3[0] . ">" . $obs;
-			canno = String.format("c.%d%c>%c", (refvarstart - refcdsstart + 1), wtnt3.charAt(0), varc);
+			cDNAAnno = String.format("c.%d%c>%c", (refVarStart - refCDSStart + 1), wtnt3.charAt(0), varc);
 			// System.out.println("wtnt3=" + wtnt3 + " varnt3=" + varnt3 + " canno=" + canno);
 			// System.out.println(kgl.getGeneSymbol() + ":" + kgl.getName());
 			// System.out.println("refvarstart=" + refvarstart + " refcdsstart = " + refcdsstart);
 			// kgl.debugPrint();
 			if (refc != wtnt3.charAt(0)) {
-				char strand = kgl.getStrand();
+				char strand = tm.getStrand();
 				wrng = String.format("WARNING:_mRNA/genome_discrepancy:_%s/%s_strand=%c", ref, wtnt3.charAt(1), strand);
 			}
 		}
-		String wtaa = translator.translateDNA(wtnt3);
-		String varaa = translator.translateDNA(varnt3);
-		int aavarpos = (int) Math.floor((refvarstart - kgl.getRefCDSStart()) / 3) + 1;
+		String wtAA = translator.translateDNA(wtnt3);
+		String varAA = translator.translateDNA(varNT3);
+		int aaVarPos = (int) Math.floor((refVarStart - tm.getRefCDSStart()) / 3) + 1;
 
-		if (wtaa.equals(varaa)) {
+		if (wtAA.equals(varAA)) {
 			// $wtaa eq '*' and ($wtaa, $varaa) = qw/X X/; #change * to X in the output NO! Not HGVS conform
 			// $function->{$index}{ssnv} .= "$geneidmap->{$seqid}:$seqid:exon$exonpos:$canno:p.$wtaa$varpos$varaa,";
-			if (wtaa.equals(varaa))
-				panno = String.format("%s:exon%d:%s:p.=", kgl.getName(), exonNumber, canno);
+			if (wtAA.equals(varAA))
+				protAnno = String.format("%s:exon%d:%s:p.=", tm.getName(), exonNumber, cDNAAnno);
 			else
-				panno = String.format("%s:exon%d:%s:p.%s%d%s", kgl.getName(), exonNumber, canno, wtaa, aavarpos, varaa);
-			vartype = VariantType.SYNONYMOUS;
-		} else if (varaa.equals("*")) {
+				protAnno = String.format("%s:exon%d:%s:p.%s%d%s", tm.getName(), exonNumber, cDNAAnno, wtAA, aaVarPos,
+						varAA);
+			varType = VariantType.SYNONYMOUS;
+		} else if (varAA.equals("*")) {
 			// $function->{$index}{stopgain} .= "$geneidmap->{$seqid}:$seqid:exon$exonpos:$canno:p.$wtaa${varpos}X,";
-			panno = String.format("%s:exon%d:%s:p.%s%d*", kgl.getName(), exonNumber, canno, wtaa, aavarpos);
-			vartype = VariantType.STOPGAIN;
-		} else if (wtaa.equals("*")) {
+			protAnno = String.format("%s:exon%d:%s:p.%s%d*", tm.getName(), exonNumber, cDNAAnno, wtAA, aaVarPos);
+			varType = VariantType.STOPGAIN;
+		} else if (wtAA.equals("*")) {
 			/* i.e., the wildtype codon is the stop codon */
-			panno = String.format("%s:exon%d:%s:p.*%d%s", kgl.getName(), exonNumber, canno, aavarpos, varaa);
-			vartype = VariantType.STOPLOSS;
+			protAnno = String.format("%s:exon%d:%s:p.*%d%s", tm.getName(), exonNumber, cDNAAnno, aaVarPos, varAA);
+			varType = VariantType.STOPLOSS;
 		} else { /* Missense */
 			// $function->{$index}{nssnv} .= "$geneidmap->{$seqid}:$seqid:exon$exonpos:$canno:p.$wtaa$varpos$varaa,";
-			panno = String.format("%s:exon%d:%s:p.%s%d%s", kgl.getName(), exonNumber, canno, wtaa, aavarpos, varaa);
-			vartype = VariantType.MISSENSE;
+			protAnno = String
+					.format("%s:exon%d:%s:p.%s%d%s", tm.getName(), exonNumber, cDNAAnno, wtAA, aaVarPos, varAA);
+			varType = VariantType.MISSENSE;
 
 		}
 		if (wrng != null)
-			panno = String.format("%s:[%s]", panno, wrng);
-		ann = new Annotation(kgl, panno, vartype, cdspos);
-		return ann;
+			protAnno = String.format("%s:[%s]", protAnno, wrng);
+		return new Annotation(tm, protAnno, varType, cdsPos);
 	}
 
 	/**
 	 * Creates annotation for a single-nucleotide substitution on the minus strand.
-	 * 
+	 *
 	 * @param kgl
 	 *            The known gene that corresponds to the deletion caused by the variant.
 	 * @param frame_s
