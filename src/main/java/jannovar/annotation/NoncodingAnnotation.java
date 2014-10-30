521d7e46ce6497bdb5b53af63df15e98898a8494
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2014-10-30 11:21:06 +0100

    Updating variable names in NoncodingAnnotation, some reformatting.

--- src/main/java/jannovar/annotation/NoncodingAnnotation.java
+++ src/main/java/jannovar/annotation/NoncodingAnnotation.java
@@ -9,15 +9,14 @@ import jannovar.reference.TranscriptModel;
  * @version 0.04 (15 April, 2014)
  * @author Peter N Robinson, Marten Jäger
  */
-
 public class NoncodingAnnotation {
 
 	/**
 	 * Return an annotation for a UTR3 mutation for a gene.
 	 * 
-	 * @param trmdl
+	 * @param tm
 	 *            Gene with splice mutation for current chromosomal variant.
-	 * @param rvarstart
+	 * @param rVarStart
 	 *            start position of the variant
 	 * @param ref
 	 *            reference sequence
@@ -27,12 +26,12 @@ public class NoncodingAnnotation {
 	 *            number of the exon affected by the variant (zero-based).
 	 * @return An {@link jannovar.annotation.Annotation Annotation} object corresponding to a 3' UTR variant.
 	 */
-	public static Annotation createNoncodingExonicAnnotation(TranscriptModel trmdl, int rvarstart, String ref, String alt, int exonNumber) {
+	public static Annotation createNoncodingExonicAnnotation(TranscriptModel tm, int rVarStart, String ref, String alt, int exonNumber) {
 		// shift
 		if (!(ref.length() == 1 && alt.length() == 1 && !alt.equals("-"))) { // only for non singlenucleotide
 																				// substitutions
-			while (rvarstart + ref.length() < trmdl.getCDNASequence().length() && trmdl.getCDNASequence().charAt(rvarstart - 1) == trmdl.getCDNASequence().charAt(rvarstart + ref.length() - 1)) {
-				rvarstart++;
+			while (rVarStart + ref.length() < tm.getCDNASequence().length() && tm.getCDNASequence().charAt(rVarStart - 1) == tm.getCDNASequence().charAt(rVarStart + ref.length() - 1)) {
+				rVarStart++;
 				ref = new StringBuilder().append(ref.substring(1)).append(ref.charAt(0)).toString();
 			}
 		}
@@ -42,25 +41,25 @@ public class NoncodingAnnotation {
 		if (alt.equals("-")) {
 			/* i.e., deletion  */
 			if (ref.length() == 1) {
-				annotation = String.format("%s:exon%d:n.%ddel", trmdl.getName(), exonNumber, rvarstart);
+				annotation = String.format("%s:exon%d:n.%ddel", tm.getName(), exonNumber, rVarStart);
 			} else {
-				int d2 = rvarstart + ref.length() - 1;
-				annotation = String.format("%s:exon%d:n.%d_%ddel", trmdl.getName(), exonNumber, rvarstart, d2);
+				int d2 = rVarStart + ref.length() - 1;
+				annotation = String.format("%s:exon%d:n.%d_%ddel", tm.getName(), exonNumber, rVarStart, d2);
 			}
 		} else if (ref.equals("-")) {
 			/* i.e., insertion */
-			int d2 = rvarstart + 1; /* get end of insertion */
-			annotation = String.format("%s:exon%d:n.%d_%dins%s", trmdl.getName(), exonNumber, rvarstart, d2, alt);
+			int d2 = rVarStart + 1; /* get end of insertion */
+			annotation = String.format("%s:exon%d:n.%d_%dins%s", tm.getName(), exonNumber, rVarStart, d2, alt);
 
 		} else {
 			/* i.e., substitution. The following code will be correct for SNPs, and may need to 
 			   be improved in the future for block substitutions. */
-			annotation = String.format("%s:exon%d:n.%d%s>%s", trmdl.getName(), exonNumber, rvarstart, ref, alt);
+			annotation = String.format("%s:exon%d:n.%d%s>%s", tm.getName(), exonNumber, rVarStart, ref, alt);
 
 		}
-		Annotation ann = new Annotation(trmdl, annotation, VariantType.ncRNA_EXONIC, rvarstart);
-		ann.setGeneID(trmdl.getGeneID());
-		ann.setGeneSymbol(trmdl.getGeneSymbol());
+		Annotation ann = new Annotation(tm, annotation, VariantType.ncRNA_EXONIC, rVarStart);
+		ann.setGeneID(tm.getGeneID());
+		ann.setGeneSymbol(tm.getGeneSymbol());
 		return ann;
 	}
 
