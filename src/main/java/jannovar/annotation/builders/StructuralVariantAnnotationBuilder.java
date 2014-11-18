37768e9390311cd3fe41bb600a396d5f07f4f953
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2014-11-18 11:31:39 +0100

    Adding annotation builder for SVs.

---
+++ src/main/java/jannovar/annotation/builders/StructuralVariantAnnotationBuilder.java
@@ -0,0 +1,103 @@
+package jannovar.annotation.builders;
+
+import jannovar.annotation.Annotation;
+import jannovar.common.VariantType;
+import jannovar.exception.InvalidGenomeChange;
+import jannovar.reference.GenomeChange;
+import jannovar.reference.GenomePosition;
+import jannovar.reference.PositionType;
+import jannovar.reference.TranscriptInfo;
+
+/**
+ * Class providing static functions for creating {@link Annotation} objects for SVs.
+ *
+ * @author Manuel Holtgrewe <manuel.holtgrewe@charite.de>
+ */
+public class StructuralVariantAnnotationBuilder {
+
+	/**
+	 * Returns a {@link Annotation} for the structural variant {@link GenomeChange} in the given {@link TranscriptInfo}.
+	 *
+	 * @param transcript
+	 *            {@link TranscriptInfo} for the transcript to compute the affection for, use <code>null</code> for
+	 *            intergenic variants
+	 * @param change
+	 *            {@link GenomeChange} to compute the annotation for, must describe a structural variant affecting
+	 *            <code>transcript</code>
+	 * @return annotation for the given change to the given transcript
+	 *
+	 * @throws InvalidGenomeChange
+	 *             if there are problems with the position or variant in <code>change</code>
+	 */
+	public static Annotation buildAnnotation(TranscriptInfo transcript, GenomeChange change) throws InvalidGenomeChange {
+		// Project the change to the forward strand.
+		change = change.withStrand('+');
+
+		// Forward everything to the helper.
+		return getStructuralVariantAnnotation(transcript, change);
+	}
+
+	private static Annotation getStructuralVariantAnnotation(TranscriptInfo transcript, GenomeChange change) {
+		// Obtain shortcuts.
+		GenomePosition position = change.getPos().withPositionType(PositionType.ZERO_BASED);
+		final int beginPos = position.getPos();
+		final String ref = change.getRef();
+		final String alt = change.getAlt();
+
+		// Build reverse-complement of alt string.
+		StringBuilder altRC = new StringBuilder(alt).reverse();
+		for (int i = 0; i < altRC.length(); ++i)
+			if (altRC.charAt(i) == 'A')
+				altRC.setCharAt(i, 'T');
+			else if (altRC.charAt(i) == 'T')
+				altRC.setCharAt(i, 'A');
+			else if (altRC.charAt(i) == 'C')
+				altRC.setCharAt(i, 'G');
+			else if (altRC.charAt(i) == 'G')
+				altRC.setCharAt(i, 'C');
+
+		// TODO(holtgrem): we should care about breakpoints within genes
+
+		if (ref.length() == alt.length() && ref.equals(altRC.toString())) { // SV inversion
+			if (transcript == null) {
+				return new Annotation(null, String.format("%s:g.%d_%dinv", VariantType.INTERGENIC, beginPos + 1,
+						beginPos + ref.length()), VariantType.INTERGENIC);
+			} else {
+				return new Annotation(transcript.transcriptModel, String.format("%s:g.%d_%dinv",
+						VariantType.SV_INVERSION, beginPos + 1, beginPos + ref.length()), VariantType.SV_INVERSION);
+
+			}
+		} else if (ref.length() == 0) { // SV insertion
+			// if transcript is null it is intergenic
+			if (transcript == null) {
+				return new Annotation(null, String.format("%s:g.%d_%dins%s..%s",
+						VariantType.INTERGENIC, beginPos, beginPos + 1, alt.substring(0, 2),
+						alt.substring(alt.length() - 2, alt.length())), VariantType.INTERGENIC);
+			} else {
+				return new Annotation(transcript.transcriptModel, String.format("%s:g.%d_%dins%s..%s",
+						VariantType.SV_INSERTION, beginPos, beginPos + 1, alt.substring(0, 2),
+						alt.substring(alt.length() - 2, alt.length())), VariantType.SV_INSERTION);
+			}
+		} else if (alt.length() == 0) { // SV deletion
+			// if tm is null it is intergenic
+			if (transcript == null) {
+				return new Annotation(null, String.format("%s:g.%d_%ddel",
+						VariantType.INTERGENIC, beginPos + 1, beginPos + ref.length()), VariantType.INTERGENIC);
+			} else {
+				return new Annotation(transcript.transcriptModel, String.format("%s:g.%d_%ddel",
+						VariantType.SV_DELETION, beginPos + 1, beginPos + ref.length()), VariantType.SV_DELETION);
+			}
+		} else { // SV substitution
+			// if tm is null it is intergenic
+			if (transcript == null) {
+				return new Annotation(null, String.format("%s:g.%d_%ddelins%s..%s",
+						VariantType.INTERGENIC, beginPos + 1, beginPos + ref.length(), alt.substring(0, 2),
+						alt.substring(alt.length() - 2, alt.length())), VariantType.INTERGENIC);
+			} else {
+				return new Annotation(transcript.transcriptModel, String.format("%s:g.%d_%ddelins%s..%s",
+						VariantType.SV_SUBSTITUTION, beginPos + 1, beginPos + ref.length(), alt.substring(0, 2),
+						alt.substring(alt.length() - 2, alt.length())), VariantType.SV_SUBSTITUTION);
+			}
+		}
+	}
+}
---
+++ src/test/java/jannovar/annotation/builders/StructuralVariantAnnotationBuilderTest.java
@@ -0,0 +1,119 @@
+package jannovar.annotation.builders;
+
+import jannovar.annotation.Annotation;
+import jannovar.exception.InvalidGenomeChange;
+import jannovar.reference.GenomeChange;
+import jannovar.reference.GenomePosition;
+import jannovar.reference.PositionType;
+import jannovar.reference.TranscriptInfo;
+import jannovar.reference.TranscriptModel;
+import jannovar.reference.TranscriptModelFactory;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the StructuralVariantAnnotationBuilder.
+ *
+ * @author Manuel Holtgrewe <manuel.holtgrewe@charite.de>
+ */
+public class StructuralVariantAnnotationBuilderTest {
+
+	// TODO(holtgrem): The shared models/infos could go into a *Test super class.
+
+	/** transcript on forward strand */
+	TranscriptModel transcriptForward;
+	/** transcript on reverse strand */
+	TranscriptModel transcriptReverse;
+	/** transcript info on forward strand */
+	TranscriptInfo infoForward;
+	/** transcript info on reverse strand */
+	TranscriptInfo infoReverse;
+
+	@Before
+	public void setUp() {
+		this.transcriptForward = TranscriptModelFactory
+				.parseKnownGenesLine("uc001anx.3	chr1	+	6640062	6649340	6640669	6649272	11	6640062,6640600,6642117,6645978,6646754,6647264,6647537,6648119,6648337,6648815,6648975,	6640196,6641359,6642359,6646090,6646847,6647351,6647692,6648256,6648502,6648904,6649340,	P10074	uc001anx.3");
+		this.transcriptForward
+				.setSequence("cgtcacgtccggcgcggagacggtggagtctccgcactgtcggcggggtacgcatagccgggcactaggttcgtgggctgtggaggcgacggagcagggggccagtggggccagctcagggaggacctgcctgggagctttctcttgcataccctcgcttaggctggccggggtgtcacttctgcctccctgccctccagaccatggacggctccttcgtccagcacagtgtgagggttctgcaggagctcaacaagcagcgggagaagggccagtactgcgacgccactctggacgtggggggcctggtgtttaaggcacactggagtgtccttgcctgctgcagtcactttttccagagcctctacggggatggctcagggggcagtgtcgtcctccctgctggcttcgctgagatctttggcctcttgttggactttttctacactggtcacctcgctctcacctcagggaaccgggatcaggtgctcctggcagccagggagttgcgagtgccagaggccgtagagctgtgccagagcttcaagcccaaaacttcagtgggacaggcagcaggtggccagagtgggctggggccccctgcctcccagaatgtgaacagccacgtcaaggagccggcaggcttggaagaagaggaagtttcgaggactctgggtctagtccccagggatcaggagcccagaggcagtcatagtcctcagaggccccagctccattccccagctcagagtgagggcccctcctccctctgtgggaaactgaagcaggccttgaagccttgtccccttgaggacaagaaacccgaggactgcaaagtgcccccaaggcccttagaggctgaaggtgcccagctgcagggcggcagtaatgagtgggaagtggtggttcaagtggaggatgatggggatggcgattacatgtctgagcctgaggctgtgctgaccaggaggaagtcaaatgtaatccgaaagccctgtgcagctgagccagccctgagcgcgggctccctagcagctgagcctgctgagaacagaaaaggtacagcggtgccggtcgaatgccccacatgtcataaaaagttcctcagcaaatattatctaaaagtccacaacaggaaacatactggggagaaaccctttgagtgtcccaaatgtgggaagtgttactttcggaaggagaacctcctggagcatgaagcccggaattgcatgaaccgctcggaacaggtcttcacgtgctctgtgtgccaggagacattccgccgaaggatggagctgcgggtgcacatggtgtctcacacaggggagatgccctacaagtgttcctcctgctcccagcagttcatgcagaagaaggacttgcagagccacatgatcaaacttcatggagcccccaagccccatgcatgccccacctgtgccaagtgcttcctgtctcggacagagctgcagctgcatgaagctttcaagcaccgtggtgagaagctgtttgtgtgtgaggagtgtgggcaccgggcctcgagccggaatggcctgcagatgcacatcaaggccaagcacaggaatgagaggccacacgtatgtgagttctgcagccacgccttcacccaaaaggccaatctcaacatgcacctgcgcacacacacgggtgagaagcccttccagtgccacctctgtggcaagaccttccgaacccaagccagcctggacaagcacaaccgcacccacaccggggaaaggcccttcagttgcgagttctgtgaacagcgcttcactgagaaggggcccctcctgaggcacgtggccagccgccatcaggagggccggccccacttctgccagatatgcggcaagaccttcaaagccgtggagcaactgcgtgtgcacgtcagacggcacaagggggtgaggaagtttgagtgcaccgagtgtggctacaagtttacccgacaggcccacctgcggaggcacatggagatccacgaccgggtagagaactacaacccgcggcagcgcaagctccgcaacctgatcatcgaggacgagaagatggtggtggtggcgctgcagccgcctgcagagctggaggtgggctcggcggaggtcattgtggagtccctggcccagggcggcctggcctcccagctccccggccagagactgtgtgcagaggagagcttcaccggcccaggtgtcctggagccctccctcatcatcacagctgctgtccccgaggactgtgacacatagcccattctggccaccagagcccacttggccccacccctcaataaaccgtgtggctttggactctcgtaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+						.toUpperCase());
+		this.transcriptForward.setGeneSymbol("ZBTB48");
+		this.infoForward = new TranscriptInfo(this.transcriptForward);
+		// RefSeq: NM_005341.3
+
+		this.transcriptReverse = TranscriptModelFactory
+				.parseKnownGenesLine("uc001bgu.3	chr1	-	23685940	23696357	23688461	23694498	4	23685940,23693534,23694465,23695858,	23689714,23693661,23694558,23696357,	Q9C0F3	uc001bgu.3");
+		this.transcriptReverse
+				.setSequence("aataagctgctatattctttttccatcacttccctctccaaggctacagcgagctgggagctcttccccacgcagaatgcctgctttccccagtgctcgacttccattgtctaattccctcatcctggctggggaaagggagagctgcgagtcctcccgttccgaggaactccagctgaatgcagcttagttgctggtggtttctcggccagcctctgtggtctcagggatctgcctatgagcctgtggtttctgagctgcctgcgagtctgaggcctcgggaatctgagtctttaggatcagcctacgatatctgggcttcgcctgcaagtctacgaattcgagatctacctgcgggtctgagacctccgggacctgcccgtgctctctagaatcttcctgaacgccaggtctgagagaacgctgcggctctggaacccgttcgcggtctctcaggttttggagacgacgatctagtggatcttttgcgggacaggagcgctgtctgctagctgcttttcctgctctctctccctggaggcgaacccttgtgctcgagatggcagccaccctgctcatggctgggtcccaggcacctgtgacgtttgaagatatggccatgtatctcacccgggaagaatggagacctctggacgctgcacagagggacctttaccgggatgttatgcaggagaattatggaaatgttgtctcactagattttgagatcaggagtgagaacgaggtaaatcccaagcaagagattagtgaagatgtacaatttgggactacatctgaaagacctgctgagaatgctgaggaaaatcctgaaagtgaagagggctttgaaagcggagataggtcagaaagacaatggggagatttaacagcagaagagtgggtaagctatcctctccaaccagtcactgatctacttgtccacaaagaagtccacacaggcatccgctatcatatatgttctcattgtggaaaggccttcagtcagatctcagaccttaatcgacatcagaagacccacactggagacagaccctataaatgttatgaatgtggaaaaggcttcagtcgcagctcacaccttattcagcatcaaagaacacatactggggagaggccttatgactgtaacgagtgtgggaaaagttttggaagaagttctcacctgattcagcatcagacaatccacactggagagaagcctcacaaatgtaatgagtgtggaaaaagtttctgccgtctctctcacctaatccaacaccaaaggacccacagtggtgagaaaccctatgagtgtgaggagtgtgggaaaagcttcagccggagctctcacctagctcagcaccagaggacccacacgggtgagaaaccttatgaatgtaacgaatgtggccgaggcttcagtgagagatctgatctcatcaaacactatcgagtccacacaggggagaggccctacaagtgtgatgagtgtgggaagaatttcagtcagaactccgaccttgtgcgtcatcgcagagcccacacgggagagaagccataccactgtaacgaatgtggggaaaatttcagccgcatctcacacttggttcagcaccagagaactcacactggagagaagccatatgaatgcaatgcttgtgggaaaagcttcagccggagctctcatctcatcacacaccagaaaattcacactggagagaagccttatgagtgtaatgagtgttggcgaagctttggtgaaaggtcagatctaattaaacatcagagaacccacacaggggagaagccctacgagtgtgtgcagtgtgggaaaggtttcacccagagctccaacctcatcacacatcaaagagttcacacgggagagaaaccttatgaatgtaccgaatgtgagaagagtttcagcaggagctcagctcttattaaacataagagagttcatacggactaagctgtaattatgatggctgagaaatgattcatttgaagatacaattttatttgatatcaatgaacgccctcaagactgagctgcttttatcatactctcctagttgtgggccacgatttaaaccatcagagatgacaagccatttgaaattctgaccctcagctttgggaatgttatctcctccaaaatggtgatttttattcactcaatgggttacttcattaaaagcagccccacaagtaactggaaatctgaagaccaggggacaaatgctggtgaatgcttaggcctggaaatggagtaaatctttcaatgttattttctcccatccttggcccaaggaactatgctaagtgaaacgtgggactgtaatagggtggtaatggctgctttggaaaaaggcaactagagactctgcctaaattgccacacctattcacacaccatagtagttgggcacacacatcttcccttccaaagggctttttccttgagttgctcatgcatttgtatcttttccatcttcctgagggcaagattttgcacgatgaaggcaatgattgtaacttttctccttctcattgtttctaattagctcctttaaagcttgcatctttgtgaaggctaactgaagatacggttggaaaggaaaaatgagacacaggtttggggaccaaggacccatcaatgatggtgactttagcagaagatgcccacagttattactgccattaatcagatttatgaattttctttggggatcactatagggaatattgtatagaaaatatcttcaagaaaagataggaccatcagtgacagttaagtgtaaggagcaagtggaattgagtccttcagggaaggaaccacagagtcccttcccaaggaatgtaggtcgtttctgtgttctttcccttctaatctttaagatcaactcttcctatcctgctaactctaagatttgataagggccacatcccagtgtttatcttagcttgcatcagggcatgtgtatgtacagtaatgtgtattcctgtggtttttctaatagaaactgaatttacagagacttagcatgttcttgggtgatgtgagtcatgtgacagaagtacagacataactccaatgtgagaaatgtccttttttcattatggaaaataatttaaacactagtgctttagtgtgcactctcctgtaaggtctgtctttgtacagagctaagcacttgtttgtatgtgtttgtcaattgtggaagataatgaccagacaaataggtcgattgtcctattctcagaatgaattatcttctatggtaatgaagaactctttggcttagtcagaaggaattaacgaacctcggtaggaatgtatttccatcctcccaccctacagatataagaggttaaaataacagttcgcccaatttaagcccagtagtgtcagttttcctaatctcagtccaggtaggaattaagaaatatctcaagtgttgatgctatccaagcatgttggggtggaagggaattggtgcccagaaaatgggactggagtgaggaatatcttttcttttgagagtacccccagtttatttctactgtgctttattgctactgttctttattgtgaatgttgtaacattttaaaaatgttttgccatagctttttaggacttggtgttaaaggagccagtggtctctctgggtgggtactataatgagttattgtgacccacagctgtgtgggaccacatcacttgttaataacacaacctttaaagtaacccatcttccaggggggttccttcatgttgccactcctttttaaggacaaactcaggcaaggagcatgtttttttgttatttacaaaatctagcagactgtgggtatccatattttaattgtcgggtgacacatgttcttggtaactaaactcaaatatgtcttttctcatatatgttgctgatggttttaataaatgtcaaagttctcctgttgcttctgtgagccactatgggtatcagcttgggagtggccatagatgaccgcatttccatgacctaactgtatttcacccccttttccttccctactgttcttgccccaccccaaccagttcctgctgctgcttttggcttcttggaggtgaagggcttaaaacaaggcttctaagcacccagctatctccatacatgaacaatctagctgggaaacttaagggacaagggccacaccagctgtctcctctttctgccaattgttgcccgtttgctgtgttgaactttgtatagaactcatgcatcagactcccttcactaatgctttttgcatgccttctgctcccaagtccctggctgcctctgcacatcccgtgaacactttgtgcctgttttctatggttgtggagaattaatgaacaaatcaatatgtagaacagttttccttatggtattggtcacagttatcctagtgtttgtattattctaacaatattctataattaaaaatataatttttaaagtca"
+						.toUpperCase());
+		this.transcriptReverse.setGeneSymbol("ZNF436");
+		this.infoReverse = new TranscriptInfo(this.transcriptReverse);
+		// RefSeq: NM_001077195.1
+	}
+
+	@Test
+	public void testSVInsertionOnTranscript() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED), "",
+				"CGAT");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(infoForward, change);
+		Assert.assertEquals("SV_INSERTION:g.6640062_6640063insCG..AT", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVInsertionIntergenic() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED), "",
+				"CGAT");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(null, change);
+		Assert.assertEquals("INTERGENIC:g.6640062_6640063insCG..AT", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVDeletionOnTranscript() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED), "",
+				"CGAT");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(infoForward, change);
+		Assert.assertEquals("SV_INSERTION:g.6640062_6640063insCG..AT", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVDeletionIntergenic() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED),
+				"CGAT", "");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(null, change);
+		Assert.assertEquals("INTERGENIC:g.6640063_6640066del", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVInversionOnTranscript() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED),
+				"CGAT", "ATCG");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(infoForward, change);
+		Assert.assertEquals("SV_INVERSION:g.6640063_6640066inv", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVInversionIntergenic() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED),
+				"CGAT", "ATCG");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(null, change);
+		Assert.assertEquals("INTERGENIC:g.6640063_6640066inv", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVSubstitutionOnTranscript() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED),
+				"CGAT", "TTTTA");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(infoForward, change);
+		Assert.assertEquals("SV_SUBSTITUTION:g.6640063_6640066delinsTT..TA", anno.getVariantAnnotation());
+	}
+
+	@Test
+	public void testSVSubstitutionIntergenic() throws InvalidGenomeChange {
+		final GenomeChange change = new GenomeChange(new GenomePosition('+', 1, 6640062, PositionType.ZERO_BASED),
+				"CGAT", "TTTTA");
+		final Annotation anno = StructuralVariantAnnotationBuilder.buildAnnotation(null, change);
+		Assert.assertEquals("INTERGENIC:g.6640063_6640066delinsTT..TA", anno.getVariantAnnotation());
+	}
+
+}
