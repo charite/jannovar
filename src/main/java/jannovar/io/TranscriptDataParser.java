a04d3c1ffbf929120bae5db27413100fb581f71a
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2014-12-05 09:33:09 +0100

    Removing unused code.

--- src/main/java/jannovar/io/TranscriptDataParser.java
+++ src/main/java/jannovar/io/TranscriptDataParser.java
@@ -1,19 +1,13 @@
 package jannovar.io;
 
-import jannovar.exception.KGParseException;
 import jannovar.reference.TranscriptModel;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
@@ -125,82 +119,6 @@ public class TranscriptDataParser {
 		} else {
 			directory.mkdir();
 		}
-	}
-
-	/**
-	 * This method downloads a file to the specified local file path. If the file already exists, it emits a warning
-	 * message and does nothing.
-	 *
-	 * @param baseURL
-	 *            remote directory path
-	 * @param fname
-	 *            remote file name
-	 * @return <code>true</code> if file was downloaded successfully
-	 * @throws jannovar.exception.KGParseException
-	 */
-	public boolean download_file(String baseURL, String fname) throws KGParseException {
-
-		String urlstring = baseURL + fname;
-		String local_file_path = this.basePath + fname;
-		File f = new File(local_file_path);
-		if (f.exists()) {
-			System.err.println(String.format("[INFO] Timorously refusing to download " + "file \"%s\" since it already exists", local_file_path));
-			return false;
-		}
-		System.err.println("[INFO] Downloading: \"" + urlstring + "\"");
-
-		// Error handling can be improved with Java 7.
-		String err = null;
-		InputStream reader = null;
-		FileOutputStream writer = null;
-
-		int threshold = 0;
-		int block = 250000;
-		try {
-			URL url = new URL(urlstring);
-			URLConnection urlc = url.openConnection();
-			reader = urlc.getInputStream();
-			writer = new FileOutputStream(local_file_path);
-			byte[] buffer = new byte[153600];
-			int totalBytesRead = 0;
-			int bytesRead;
-			int size = urlc.getContentLength();
-			if (size >= 0)
-				block = size / 20;
-			System.err.println("0%       50%      100%");
-			while ((bytesRead = reader.read(buffer)) > 0) {
-				writer.write(buffer, 0, bytesRead);
-				buffer = new byte[153600];
-				totalBytesRead += bytesRead;
-				if (totalBytesRead > threshold) {
-					System.err.print("=");
-					threshold += block;
-				}
-			}
-			System.err.println();
-			System.err.println("[INFO] Done. " + (new Integer(totalBytesRead).toString()) + "(" + size + ") bytes read.");
-		} catch (MalformedURLException e) {
-			err = String.format("Could not interpret url: \"%s\"\n%s", urlstring, e.toString());
-		} catch (IOException e) {
-			err = String.format("IO Exception reading from URL: \"%s\"\n%s", urlstring, e.toString());
-		} finally {
-			try {
-				if (writer != null)
-					writer.close();
-			} catch (IOException e) {
-				// swallow, nothing we can do about it
-			}
-			try {
-				if (reader != null)
-					reader.close();
-			} catch (IOException e) {
-				// swallow, nothing we can do about it
-			}
-		}
-
-		if (err != null)
-			throw new KGParseException(err);
-		return true;
 	}
 
 }
