2d77828e62113aef92b55ce59da1e9f554057935
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2015-01-06 13:22:54 +0100

    Adding Disease.toInt

--- jannovar-core/src/main/java/jannovar/pedigree/Disease.java
+++ jannovar-core/src/main/java/jannovar/pedigree/Disease.java
@@ -4,6 +4,7 @@ package jannovar.pedigree;
  * Codes used to denote affection status of a {@link Person} in a {@link Pedigree}.
  *
  * @author Peter N Robinson <peter.robinson@charite.de>
+ * @author Manuel Holtgrewe <manuel.holtgrewe@charite.de>
  */
 public enum Disease {
 	/** corresponds to 2 = affected in the pedfile. */
@@ -12,4 +13,18 @@ public enum Disease {
 	UNAFFECTED,
 	/** corresponds to 0 = unknown disease status in the pedfile. */
 	UNKNOWN;
+
+	/**
+	 * @return <code>int</code> value representation for PED file.
+	 */
+	public int toInt() {
+		switch (this) {
+		case AFFECTED:
+			return 2;
+		case UNAFFECTED:
+			return 1;
+		default:
+			return 0;
+		}
+	}
 }
