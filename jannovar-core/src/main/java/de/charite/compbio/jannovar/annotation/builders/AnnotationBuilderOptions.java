1f35b2b8c01b8f6c434a2797e6a36226543a3ceb
Manuel Holtgrewe <manuel.holtgrewe@charite.de>
2015-04-27 19:11:23 +0200

    Changing some public final into private final members.

--- jannovar-core/src/main/java/de/charite/compbio/jannovar/annotation/builders/AnnotationBuilderOptions.java
+++ jannovar-core/src/main/java/de/charite/compbio/jannovar/annotation/builders/AnnotationBuilderOptions.java
@@ -7,7 +7,7 @@ package de.charite.compbio.jannovar.annotation.builders;
  */
 public class AnnotationBuilderOptions {
 	/** whether or not to shift variants towards the 3' end of the transcript (default is <code>true</code>) */
-	public final boolean nt3PrimeShifting;
+	private final boolean nt3PrimeShifting;
 
 	public AnnotationBuilderOptions() {
 		this.nt3PrimeShifting = true;
@@ -16,4 +16,6 @@ public class AnnotationBuilderOptions {
 	public AnnotationBuilderOptions(boolean nt3PrimeShifting) {
 		this.nt3PrimeShifting = nt3PrimeShifting;
 	}
+	
+	
 }
