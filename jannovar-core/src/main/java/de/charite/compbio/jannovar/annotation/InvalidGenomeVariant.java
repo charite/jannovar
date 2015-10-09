a10959fb7592d5c25c4666cd58def888db0f7e13
Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
2015-10-10 00:38:26 +0200

    Fixing import

--- jannovar-core/src/main/java/de/charite/compbio/jannovar/annotation/InvalidGenomeVariant.java
+++ jannovar-core/src/main/java/de/charite/compbio/jannovar/annotation/InvalidGenomeVariant.java
@@ -1,5 +1,7 @@
 package de.charite.compbio.jannovar.annotation;
 
+import de.charite.compbio.jannovar.reference.GenomeVariant;
+
 /**
  * Thrown when the the given {@link GenomeVariant} does not fit the used annotation builder class.
  *
