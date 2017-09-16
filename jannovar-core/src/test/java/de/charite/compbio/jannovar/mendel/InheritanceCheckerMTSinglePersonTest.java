134d8b33581ada5f09242ace41aca5c81f155513
pnrobinson <peter.robinson@charite.de>
2017-09-16 09:59:11 -0400

    Adding new test class for mitochondrion.

---
+++ jannovar-core/src/test/java/de/charite/compbio/jannovar/mendel/InheritanceCheckerMTSinglePersonTest.java
@@ -0,0 +1,38 @@
+package de.charite.compbio.jannovar.mendel;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import de.charite.compbio.jannovar.pedigree.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+public class InheritanceCheckerMTSinglePersonTest  extends MendelianCompatibilityCheckerTestBase {
+
+	MendelianInheritanceChecker checker;
+	List<GenotypeCalls> gcList;
+	ImmutableMap<ModeOfInheritance, ImmutableList<GenotypeCalls>> result;
+
+	@Before
+	public void setUp() throws Exception {
+		ImmutableList.Builder<PedPerson> individuals = new ImmutableList.Builder<PedPerson>();
+		individuals.add(new PedPerson("ped", "I.1", "0", "0", Sex.FEMALE, Disease.AFFECTED));
+		PedFileContents pedFileContents = new PedFileContents(new ImmutableList.Builder<String>().build(),
+			individuals.build());
+		this.pedigree = new Pedigree(pedFileContents, "ped");
+
+		this.names = ImmutableList.of("I.1");
+
+		this.checker = new MendelianInheritanceChecker(this.pedigree);
+
+		this.result = null;
+		this.gcList = null;
+	}
+
+	@Test
+	public void testSizeOfPedigree() {
+		Assert.assertEquals(1, pedigree.getMembers().size());
+	}
+}
