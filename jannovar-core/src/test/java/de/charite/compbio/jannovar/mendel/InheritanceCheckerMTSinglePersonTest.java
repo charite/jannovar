30bee57b9503b732f092c019058f043928bb553a
pnrobinson <peter.robinson@jax.org>
2017-09-18 10:51:04 -0400

    NEw code for test class

--- jannovar-core/src/test/java/de/charite/compbio/jannovar/mendel/InheritanceCheckerMTSinglePersonTest.java
+++ jannovar-core/src/test/java/de/charite/compbio/jannovar/mendel/InheritanceCheckerMTSinglePersonTest.java
@@ -8,6 +8,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class InheritanceCheckerMTSinglePersonTest  extends MendelianCompatibilityCheckerTestBase {
 
@@ -34,5 +36,43 @@ public class InheritanceCheckerMTSinglePersonTest  extends MendelianCompatibilit
 	@Test
 	public void testSizeOfPedigree() {
 		Assert.assertEquals(1, pedigree.getMembers().size());
+	}
+
+    	protected List<GenotypeCalls> getMitochondrialGenotypeCallsList(ImmutableList<SimpleGenotype> genotypes,
+			boolean isMitochondrial) {
+		HashMap<String, Genotype> entries = new HashMap<String, Genotype>();
+		for (int i = 0; i < names.size(); ++i) {
+			switch (genotypes.get(i)) {
+			case HET:
+				entries.put(names.get(i), new Genotype(ImmutableList.of(Genotype.REF_CALL, 1)));
+				break;
+			case REF:
+				entries.put(names.get(i), new Genotype(ImmutableList.of(Genotype.REF_CALL, Genotype.REF_CALL)));
+				break;
+			case ALT:
+				entries.put(names.get(i), new Genotype(ImmutableList.of(1, 1)));
+				break;
+			case UKN:
+				entries.put(names.get(i), new Genotype(ImmutableList.of(Genotype.NO_CALL, Genotype.NO_CALL)));
+				break;
+			}
+		}
+
+		List<GenotypeCalls> gcs = new ArrayList<GenotypeCalls>();
+		gcs.add(new GenotypeCalls(isMitochondrial ? ChromosomeType.MITOCHONDRIAL : ChromosomeType.AUTOSOMAL,
+				entries.entrySet()));
+		return gcs;
+	}
+
+    	@Test
+	public void testCaseNegativesOneVariant1() throws IncompatiblePedigreeException {
+		gcList = getGenotypeCallsList(lst(REF), true);
+		result = checker.checkMendelianInheritance(gcList);
+
+		Assert.assertEquals(0, result.get(ModeOfInheritance.AUTOSOMAL_DOMINANT).size());
+		Assert.assertEquals(0, result.get(ModeOfInheritance.AUTOSOMAL_RECESSIVE).size());
+		Assert.assertEquals(0, result.get(ModeOfInheritance.X_DOMINANT).size());
+		Assert.assertEquals(0, result.get(ModeOfInheritance.X_RECESSIVE).size());
+		Assert.assertEquals(1, result.get(ModeOfInheritance.ANY).size());
 	}
 }
