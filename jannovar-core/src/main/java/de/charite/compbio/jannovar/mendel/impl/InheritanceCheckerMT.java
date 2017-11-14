b3ac605ed6e3c98c85d4f08273468e4e86c571a2
Max <max.schubach@googlemail.com>
2017-11-14 17:52:32 +0100

    #362: Mitochondrial inheritance
    MT singleton fix!

--- jannovar-core/src/main/java/de/charite/compbio/jannovar/mendel/impl/InheritanceCheckerMT.java
+++ jannovar-core/src/main/java/de/charite/compbio/jannovar/mendel/impl/InheritanceCheckerMT.java
@@ -10,22 +10,29 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/** Helper class for checking a {@link GenotypeCalls} for compatibility with a pedigree and mitochondrial inheritance
-	*
-	* <h2>Compatibility Check</h2>
-	*
-	* In the case of a single individual, we require merely that a variant is located on the mitochondrion.
-	*
-	* In the case of multiple individuals, we require that all affecteds have the called variant and that there is
- 	* is no transmission from an affected father to children (this would mean that the case is definitely not related
- 	* to a mitochondrial mutation, whatever the distribution of variants may be!).
+/**
+ * Helper class for checking a {@link GenotypeCalls} for compatibility with a
+ * pedigree and mitochondrial inheritance
+ *
+ * <h2>Compatibility Check</h2>
+ *
+ * In the case of a single individual, we require merely that a variant is
+ * located on the mitochondrion.
+ *
+ * In the case of multiple individuals, we require that all affecteds have the
+ * called variant and that there is is no transmission from an affected father
+ * to children (this would mean that the case is definitely not related to a
+ * mitochondrial mutation, whatever the distribution of variants may be!).
  * <P>
- *     Note that mitochondrial inheritance is considered to be Nonmendelian, so that this class is named
- *     {@code InheritanceCheckerMT} rather than {@code MendelianCheckerMT} as the other classes in this package.
+ * Note that mitochondrial inheritance is considered to be Nonmendelian, so that
+ * this class is named {@code InheritanceCheckerMT} rather than
+ * {@code MendelianCheckerMT} as the other classes in this package.
  * </P>
-	*
-	* @author <a href="mailto:Peter.Robinson@jax.org">Peter N Robinson</a>
-	* @version 0.23 (September 15, 2017)
+ *
+ * @author <a href="mailto:Peter.Robinson@jax.org">Peter N Robinson</a>
+ * @author <a href="mailto:max.schubach@bihealth.de">Max Schubach</a>
+ * 
+ * @since version 0.24 (September 15, 2017)
  */
 public class InheritanceCheckerMT extends AbstractMendelianChecker {
 
@@ -117,7 +124,10 @@ public class InheritanceCheckerMT extends AbstractMendelianChecker {
 	 *         individual in the pedigree
 	 */
 	private boolean isCompatibleSingleton(GenotypeCalls calls) {
-		return calls.getNSamples()>0; // return true if there is at least one called variant.
+		if (calls.getNSamples() == 0)
+			return false; // no calls!
+		
+		return calls.getGenotypeBySampleNo(0).isHet() || calls.getGenotypeBySampleNo(0).isHomAlt();
 	}
 
 }
