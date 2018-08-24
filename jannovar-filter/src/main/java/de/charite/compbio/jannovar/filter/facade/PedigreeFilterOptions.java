c882eefe13d6c75a838264083bdac1bb24891582
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-08-24 17:08:34 +0200

    Making OneParentGtFiltered filter optional
    The default setting is `false` (specify
    `--one-parent-gt-filtered-filters-affected` to enable).

--- jannovar-filter/src/main/java/de/charite/compbio/jannovar/filter/facade/PedigreeFilterOptions.java
+++ jannovar-filter/src/main/java/de/charite/compbio/jannovar/filter/facade/PedigreeFilterOptions.java
@@ -7,32 +7,40 @@ package de.charite.compbio.jannovar.filter.facade;
  */
 public class PedigreeFilterOptions {
 
-
 	public static PedigreeFilterOptions buildDefaultOptions() {
-		return new PedigreeFilterOptions(1, true);
+		return new PedigreeFilterOptions(1, true, true);
 	}
 
 	/**
-	 * Maximal allelic depth of alternative allele to tolerate in parent before
-	 * flagging with ParentAd2.
+	 * Maximal allelic depth of alternative allele to tolerate in parent before flagging with
+	 * ParentAd2.
 	 */
 	private final int deNovoMaxParentAd2;
 
 	/**
-	 * Whether or not to apply {@code OneParentGtFiltered} and {@code BothParentGtFiltered} genotype filter. 
+	 * Whether or not to apply {@code OneParentGtFiltered} and {@code BothParentGtFiltered} genotype
+	 * filter.
 	 */
 	private final boolean applyParentGtFilteredFilters;
 
+	/**
+	 * Whether or not to apply the {@code OneParentGtFiltered} to the global {@code FILTER} column.
+	 */
+	private final boolean oneParentGtFilteredFiltersAffected;
 
-	public PedigreeFilterOptions(int deNovoMaxParentAd2, boolean applyParentGtFilteredFilters) {
+	public PedigreeFilterOptions(int deNovoMaxParentAd2, boolean applyParentGtFilteredFilters,
+			boolean oneParentGtFilteredFiltersAffected) {
 		this.deNovoMaxParentAd2 = deNovoMaxParentAd2;
 		this.applyParentGtFilteredFilters = applyParentGtFilteredFilters;
+		this.oneParentGtFilteredFiltersAffected = oneParentGtFilteredFiltersAffected;
 	}
 
 	@Override
 	public String toString() {
 		return "PedigreeFilterOptions [deNovoMaxParentAd2=" + deNovoMaxParentAd2
-				+ ", applyParentGtFilteredFilters=" + applyParentGtFilteredFilters + "]";
+				+ ", applyParentGtFilteredFilters=" + applyParentGtFilteredFilters
+				+ ", oneParentGtFilteredFiltersAffected=" + oneParentGtFilteredFiltersAffected
+				+ "]";
 	}
 
 	public int getDeNovoMaxParentAd2() {
@@ -41,6 +49,10 @@ public class PedigreeFilterOptions {
 
 	public boolean isApplyParentGtFilteredFilters() {
 		return applyParentGtFilteredFilters;
+	}
+
+	public boolean isOneParentGtFilteredFiltersAffected() {
+		return oneParentGtFilteredFiltersAffected;
 	}
 
 }
