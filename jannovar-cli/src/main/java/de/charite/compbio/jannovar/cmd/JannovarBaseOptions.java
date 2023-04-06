d00f9b0053c47a29c196b65c6be9da72df53ac55
JEET MEHTA <jt429386@dal.ca>
2023-04-06 20:11:04 -0300

    Refactor: extracted the setFromArgs method to individual sub methods for better reusability

--- jannovar-cli/src/main/java/de/charite/compbio/jannovar/cmd/JannovarBaseOptions.java
+++ jannovar-cli/src/main/java/de/charite/compbio/jannovar/cmd/JannovarBaseOptions.java
@@ -53,15 +53,15 @@ public class JannovarBaseOptions {
 
 		ArgumentGroup verboseGroup = parser.addArgumentGroup("Verbosity Options");
 		verboseGroup.addArgument("--report-no-progress").help("Disable progress report, more quiet mode")
-			.dest("report_progress").setDefault(true).action(Arguments.storeFalse());
+				.dest("report_progress").setDefault(true).action(Arguments.storeFalse());
 		verboseGroup.addArgument("-v", "--verbose").help("Enable verbose mode").dest("verbose").setDefault(false)
-			.action(Arguments.storeTrue());
+				.action(Arguments.storeTrue());
 		verboseGroup.addArgument("-vv", "--very-verbose").help("Enable very verbose mode").dest("very_verbose")
-			.setDefault(false).action(Arguments.storeTrue());
+				.setDefault(false).action(Arguments.storeTrue());
 
 		ArgumentGroup proxyGroup = parser.addArgumentGroup("Proxy Options");
 		proxyGroup.description("Configuration related to Proxy, note that environment variables *_proxy "
-			+ "and *_PROXY are also interpreted");
+				+ "and *_PROXY are also interpreted");
 		proxyGroup.addArgument("--http-proxy").help("Set HTTP proxy to use, if any");
 		proxyGroup.addArgument("--https-proxy").help("Set HTTPS proxy to use, if any");
 		proxyGroup.addArgument("--ftp-proxy").help("Set FTP proxy to use, if any");
@@ -74,28 +74,17 @@ public class JannovarBaseOptions {
 	 * @throws CommandLineParsingException on problems parsing a given URL
 	 */
 	public void setFromArgs(Namespace args) throws CommandLineParsingException {
-		if (args.getBoolean("verbose"))
-			this.verbosity = 2;
-		if (args.getBoolean("very_verbose"))
-			this.verbosity = 3;
+		checkVerbose(args);
 
 		Map<String, String> env = System.getenv();
 
+		checkHttpProxy(args, env);
+		checkHttpsProxy(args, env);
+		checkingFTP_Proxy(args, env);
+	}
+
+	public void checkingFTP_Proxy(Namespace args, Map<String, String> env) throws CommandLineParsingException {
 		try {
-			if (args.getString("http_proxy") != null)
-				this.httpProxy = new URL(args.getString("http_proxy"));
-			else if (env.get("HTTP_PROXY") != null)
-				this.httpProxy = new URL(env.get("HTTP_PROXY"));
-			else if (env.get("http_proxy") != null)
-				this.httpProxy = new URL(env.get("http_proxy"));
-
-			if (args.getString("https_proxy") != null)
-				this.httpsProxy = new URL(args.getString("https_proxy"));
-			else if (env.get("HTTPS_PROXY") != null)
-				this.httpsProxy = new URL(env.get("HTTPS_PROXY"));
-			else if (env.get("https_proxy") != null)
-				this.httpsProxy = new URL(env.get("https_proxy"));
-
 			if (args.getString("ftp_proxy") != null)
 				this.ftpProxy = new URL(args.getString("ftp_proxy"));
 			else if (env.get("FTP_PROXY") != null)
@@ -104,6 +93,42 @@ public class JannovarBaseOptions {
 				this.ftpProxy = new URL(env.get("ftp_proxy"));
 		} catch (MalformedURLException e) {
 			throw new CommandLineParsingException("Problem parsing URL", e);
+		}
+	}
+
+	public void checkHttpsProxy(Namespace args, Map<String, String> env) throws CommandLineParsingException {
+		try {
+			if (args.getString("https_proxy") != null)
+				this.httpsProxy = new URL(args.getString("https_proxy"));
+			else if (env.get("HTTPS_PROXY") != null)
+				this.httpsProxy = new URL(env.get("HTTPS_PROXY"));
+			else if (env.get("https_proxy") != null)
+				this.httpsProxy = new URL(env.get("https_proxy"));
+		} catch (MalformedURLException e) {
+			throw new CommandLineParsingException("Problem parsing URL", e);
+		}
+	}
+
+	public void checkHttpProxy(Namespace args, Map<String, String> env) throws CommandLineParsingException {
+		try {
+			if (args.getString("http_proxy") != null)
+				this.httpProxy = new URL(args.getString("http_proxy"));
+			else if (env.get("HTTP_PROXY") != null)
+				this.httpProxy = new URL(env.get("HTTP_PROXY"));
+			else if (env.get("http_proxy") != null)
+				this.httpProxy = new URL(env.get("http_proxy"));
+
+		} catch (MalformedURLException e) {
+			throw new CommandLineParsingException("Problem parsing URL", e);
+		}
+	}
+
+	public void checkVerbose(Namespace args) throws CommandLineParsingException {
+		if (args.getBoolean("verbose")) {
+			this.verbosity = 2;
+		}
+		if (args.getBoolean("very_verbose")) {
+			this.verbosity = 3;
 		}
 	}
 
@@ -150,7 +175,7 @@ public class JannovarBaseOptions {
 	@Override
 	public String toString() {
 		return "JannovarBaseOptions [reportProgress=" + reportProgress + ", httpProxy=" + httpProxy + ", httpsProxy="
-			+ httpsProxy + ", ftpProxy=" + ftpProxy + ", verbosity=" + verbosity + "]";
+				+ httpsProxy + ", ftpProxy=" + ftpProxy + ", verbosity=" + verbosity + "]";
 	}
 
 }
